62ac26bc1ccac83c79488f7706e5d8b3b03184ef
Gowri4622 <gowrihifi@gmail.com>
2023-04-07 18:07:18 +0530

    Changes done in Banner.

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -2,16 +2,14 @@ const Banner = () => {
     return(
       <div className="banner">
         <div className="banner__contents">
-          <h1 className="banner__title">Ginny &amp; Georgia</h1>
+          <h1 className="banner__title">Wednesday (2022)</h1>
           <div className="banner__buttons">
             <button className="banner__button">Play</button>
             <button className="banner__button">My List</button>
             <button className="banner__button ">Search</button>
           </div>
           <h1 className="banner__description">
-            Angsty and awkward fifteen year old Ginny Miller often feels more
-            mature than her thirty year old mother, the irresistible and dynamic
-            Georgia Miller...
+          Wednesday Addams is sent to Nevermore Academy, a bizarre boarding school where she attempts to master her psychic powers, stop a monstrous killing spree of the town citizens, and solve the supernatural mystery that affected her family 25 years ago — all while navigating her new relationships.
           </h1>
         </div>
         <div className="banner--fadeBottom"></div>
